39ffda03b2207f2ef208b40781c2874f4c36cc36
Brook Jordan <brkjrdn@gmail.com>
2019-01-02 14:55:07 +0800

    External site pages

---
+++ external-site-pages.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    title: 'Webinar | How to introduce a subscription model',
+    description: 'How to introduce a subscription business model & take your eCommerce business to new heights',
+    url: 'https://www.crowdcast.io/e/how-to-introduce-a/register',
+    author: '',
+    //image: '',
+    //imageAlt: '',
+  },
+
+  {
+    title: 'Pick Pack for Shopify',
+    description: 'Pick Pack for Shopify is a free tool by TradeGecko to help you simplify your order fulfilment process. Sync with your Shopify store and see all outstanding orders to pick from your iPhone or iPad.',
+    url: 'https://itunes.apple.com/us/app/pick-pack-for-shopify/id1137876236',
+    author: '',
+    //image: '',
+    //imageAlt: '',
+  },
+];
